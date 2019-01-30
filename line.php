73d6fde03b088cde76c2ab936734dc500a124588
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 21:21:56 +0700

    Update line.php

--- line.php
+++ line.php
@@ -11,7 +11,7 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        'U08ca677b3db4281d24b7c88c6c339a89',
+        'to' => 'U08ca677b3db4281d24b7c88c6c339a89',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
