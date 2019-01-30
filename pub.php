5f4010447c4ca4da93b482455dc023884c2d0248
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 17:20:21 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,49 +1,43 @@
  <?php
- function pubMqtt($topic,$msg){
-       $APPID= "demoremote/"; //enter your appid
-     $KEY = "TPQJn54Iiz94BSj"; //enter your key
+ function pubMqtt($topic,$msg) {
+    $APPID= "demoremote/";                 //enter your appid
+    $KEY = "TPQJn54Iiz94BSj";              //enter your key
     $SECRET = "FfnPDBAAmHnQ86XqHsz9bBAPJ"; //enter your secret
-    $Topic = "$topic"; 
-      put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
- 
+    $Topic = "$topic";
+  
+    put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
   }
- function getMqttfromlineMsg($Topic,$lineMsg){
- 
+
+ function getMqttfromlineMsg($Topic,$lineMsg) {
     $pos = strpos($lineMsg, ":");
-    if($pos){
+    if($pos) {
       $splitMsg = explode(":", $lineMsg);
       $topic = $splitMsg[0];
       $msg = $splitMsg[1];
       pubMqtt($topic,$msg);
-    }else{
+    }
+    else {
       $topic = $Topic;
       $msg = $lineMsg;
       pubMqtt($topic,$msg);
     }
   }
  
-  function put($url,$tmsg)
-{
-      
-    $ch = curl_init($url);
- 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  function put($url,$tmsg) {
+     $ch = curl_init($url);
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
      
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-     
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-     
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-     
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
-      
-    $response = curl_exec($ch);
-    
-      curl_close($ch);
-      echo $response . "\r\n";
+     $response = curl_exec($ch);
+     curl_close($ch);
+     echo $response . "\r\n";
     return $response;
-}
-// $Topic = "ESP32";
- //$lineMsg = "CHECK";
- //getMqttfromlineMsg($Topic,$lineMsg);
+  }
+
+  //$Topic = "ESP32";
+  //$lineMsg = "CHECK";
+  //getMqttfromlineMsg($Topic,$lineMsg);
 ?>
