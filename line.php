1cba71a0527e507ed3b81c3780db8b1ae9a9279a
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 15:11:53 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'cFGAsTVf5PTHvBrx4O8uj0m/tY4wsIQA74Sdr597EMuTlXInQ1h+BpO65pH62YWX+eDnZWhmSWRGVGlqQUcbbudAXmiH3n+l+ha8vGu5louLJ3RK47v2BjSxauoP6jO2RZyka4G8lCECQiKzWcj6ewdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U08ca677b3db4281d24b7c88c6c339a89',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
