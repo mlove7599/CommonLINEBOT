b8ca93c892acfc5f61fd9a23de131ab083f7b4b4
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 15:45:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,14 +27,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 
-			$Topic = "NodeMCU1" ;
+			$Topic = "ESP32" ;
 			getMqttfromlineMsg($Topic,$text);
 			   
 			
 		}
 	}
 }
-$Topic = "NodeMCU1" ;
+$Topic = "ESP32" ;
 $text = "Test";
 getMqttfromlineMsg($Topic,$text);
 echo "OK3";
