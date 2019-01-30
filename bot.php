d332587e94a57b555a94f30bbf0794f46156ddb7
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 17:23:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,38 +4,34 @@
 
 // Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
 
+// Parse JSON
 $events = json_decode($content, true);
+
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
-	echo "OK";
-	}
-if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-
-			$Topic = "ESP32" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
-		}
-	}
+   send_LINE($events['ESP']);
+   echo "OK";
 }
-$Topic = "ESP32" ;
-$text = "Test";
-getMqttfromlineMsg($Topic,$text);
-echo "OK3";
+
+if (!is_null($events['events'])) {
+   echo "line bot";
+   // Loop through each event
+   foreach ($events['events'] as $event) {
+	// Reply only when message sent is in 'text' format
+	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+	   // Get text sent
+	   $text = $event['message']['text'];
+	   // Get replyToken
+	   $replyToken = $event['replyToken'];
+  	   // Build message to reply back
+           $Topic = "ESP32" ;
+	   getMqttfromlineMsg($Topic,$text);
+	}
+    }
+ }
+ $Topic = "ESP32" ;
+ $text = "Test";
+ getMqttfromlineMsg($Topic,$text);
+ echo "OK3";
 ?>
