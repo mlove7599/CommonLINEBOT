ea4603d82eefc4a50b85ac90f4de71a935520efb
iamaooddy <35629310+iamaooddy@users.noreply.github.com>
2019-01-30 17:25:22 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,14 +1,13 @@
 <?php
 $access_token = 'cFGAsTVf5PTHvBrx4O8uj0m/tY4wsIQA74Sdr597EMuTlXInQ1h+BpO65pH62YWX+eDnZWhmSWRGVGlqQUcbbudAXmiH3n+l+ha8vGu5louLJ3RK47v2BjSxauoP6jO2RZyka4G8lCECQiKzWcj6ewdB04t89/1O/w1cDnyilFU=';
-
 $url = 'https://api.line.me/v1/oauth/verify';
-
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
 $result = curl_exec($ch);
 curl_close($ch);
 
