9daecefaf269ec3c5bf797f618486073b8dfaa34
mlove7599 <64851479+mlove7599@users.noreply.github.com>
2020-05-05 23:04:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 	   // Get replyToken
 	   $replyToken = $event['replyToken'];
   	   // Build message to reply back
-           $Topic = "ESP32" ;
+           $Topic = "ESP8266" ;
 	   getMqttfromlineMsg($Topic,$text);
 	}
     }
