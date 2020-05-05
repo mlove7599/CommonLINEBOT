5b6afb5f166d64727e70b7902bfb4d9e06bfee24
mlove7599 <64851479+mlove7599@users.noreply.github.com>
2020-05-05 23:05:26 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 function send_LINE($msg){
- $access_token = 'LINE_token_ID';    //PUT LINE token ID at "Channel access token (long-lived)" 
+ $access_token = 'ZAF7bbl0lOADgPhpRXpzpGYvXVLhXejgIWkwgusVLaNxqu9p4EfULpdEQYz1yFdf6/+z2YLbKXY450QS5dO1oLb4GKSvsl+Tgb7bI1trWU3ulz4WBSTi6rF9DBQAcrI2pXy2aBmxSLJnRBvwIWa6KAdB04t89/1O/w1cDnyilFU=';    //PUT LINE token ID at "Channel access token (long-lived)" 
  $messages = [
         'type' => 'text',
         'text' => $msg
@@ -10,7 +10,7 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        'to' => 'LINE_ID',         //PUT LINE ID at "Your user ID"
+        'to' => 'Ubc56224df6fd53570fc744482ab3f244',         //PUT LINE ID at "Your user ID"
         'messages' => [$messages],
       ];
       $post = json_encode($data);
